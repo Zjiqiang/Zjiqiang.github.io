ac376ad388b5db74d0ffbbab77009247c589edc0
张继强 <z_jiqiang@163.com>
2020-10-20 12:13:27 +0800

    Site updated: 2020-10-20 12:13:27

--- sw.js
+++ sw.js
@@ -26,6 +26,8 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/6.html');
 
+  precacheUrls.push('/posts/24073.html');
+
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
 
