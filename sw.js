ae08121db888cae10a997e0073ed6a734c10f414
张继强 <z_jiqiang@163.com>
2020-10-21 15:28:46 +0800

    Site updated: 2020-10-21 15:28:46

--- sw.js
+++ sw.js
@@ -26,9 +26,9 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/844.html');
 
-  precacheUrls.push('/posts/24073.html');
-
   precacheUrls.push('/posts/3079.html');
+
+  precacheUrls.push('/posts/24073.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
