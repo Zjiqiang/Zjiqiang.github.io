2d827f7050568cc41cf029168a1dba628269343e
张继强 <z_jiqiang@163.com>
2020-10-21 22:37:17 +0800

    Site updated: 2020-10-21 22:37:17

--- sw.js
+++ sw.js
@@ -26,9 +26,9 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/844.html');
 
-  precacheUrls.push('/posts/3718.html');
-
   precacheUrls.push('/posts/3079.html');
+
+  precacheUrls.push('/posts/3718.html');
 
   precacheUrls.push('/posts/24073.html');
 
