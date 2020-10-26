1c6418272712b921403fcc137f850017746b3bcc
张继强 <z_jiqiang@163.com>
2020-10-26 21:27:41 +0800

    Site updated: 2020-10-26 21:27:41

--- sw.js
+++ sw.js
@@ -26,15 +26,17 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/48682.html');
 
-  precacheUrls.push('/posts/844.html');
-
   precacheUrls.push('/posts/61223.html');
+
+  precacheUrls.push('/posts/844.html');
 
   precacheUrls.push('/posts/25941.html');
 
+  precacheUrls.push('/posts/3079.html');
+
   precacheUrls.push('/posts/3718.html');
 
-  precacheUrls.push('/posts/3079.html');
+  precacheUrls.push('/posts/3785.html');
 
   precacheUrls.push('/posts/24073.html');
 
