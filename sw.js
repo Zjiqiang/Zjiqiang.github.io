480ec445252329b45164fffd8a0d28453b583db5
张继强 <z_jiqiang@163.com>
2020-10-28 22:17:34 +0800

    Site updated: 2020-10-28 22:17:33

--- sw.js
+++ sw.js
@@ -28,17 +28,17 @@ var precacheUrls = [];
 
   precacheUrls.push('/posts/844.html');
 
-  precacheUrls.push('/posts/61223.html');
-
   precacheUrls.push('/posts/25941.html');
 
-  precacheUrls.push('/posts/3079.html');
+  precacheUrls.push('/posts/61223.html');
 
   precacheUrls.push('/posts/3718.html');
 
-  precacheUrls.push('/posts/6044.html');
+  precacheUrls.push('/posts/3079.html');
 
   precacheUrls.push('/posts/3785.html');
+
+  precacheUrls.push('/posts/6044.html');
 
   precacheUrls.push('/posts/24073.html');
 
